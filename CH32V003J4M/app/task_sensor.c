abe656d65c0a3d452ad993f35bc7233a31a7dfd8
we2116576782 <2116576782@qq.com>
2026-09-16 22:35:18 +0800

    [ feature]:添加按键状态管理

--- CH32V003J4M/app/task_sensor.c
+++ CH32V003J4M/app/task_sensor.c
@@ -1,8 +1,22 @@
 #include "task_sensor.h"
 #include "bsp_config.h"
 #include "touch.h"
+#include "key.h"
 
 touch_struct_t touch_0 = {0};
+key_struct_t key_0 = {0};
+
+///@brief 获取按键状态
+///@param void
+///@retval 0未按下
+///@retval 1已按下
+static uint8_t get_touch_key_state(void)
+{
+    uint8_t is_touching = 0;
+    touch_get_touch_state(&touch_0, &is_touching);
+
+    return is_touching;
+}
 
 /// @brief  传感器任务初始化
 /// @param  void
@@ -22,6 +36,15 @@ void task_sensor_init(void)
         .get_adc_value = bsp_adc_get_raw,
     };
     touch_init(&touch_0, &touch_initstruct);
+
+    key_init_t key_initstruct = {
+        .time_double = 500,
+        .time_long = 1000,
+        .time_repeat = 400,
+
+        .get_state = get_touch_key_state,
+    };
+    key_init(&key_0, &key_initstruct);
 }
 
 /// @brief  传感器任务
@@ -30,14 +53,16 @@ void task_sensor_init(void)
 void task_sensor(void)
 {
     static uint8_t is_en_led = 0;
-    static uint8_t last_touching = 0;
-    uint8_t is_touching = 0;
 
-    touch_get_touch_state(&touch_0, &is_touching);
-    if (is_touching == 1 && last_touching == 0)
+    key_scan(&key_0);
+
+    uint8_t is_put_dowm = 0;
+    key_check_flag(&key_0, KEY_DOWN, &is_put_dowm);
+    if (is_put_dowm == 1)
     {
         is_en_led = !is_en_led;
         bsp_tim1_ch4_set_duty(is_en_led ? 960 : 0);
     }
-    last_touching = is_touching;
+
+    Delay_Ms(1);
 }
